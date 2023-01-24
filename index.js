a093bee5207f566906f1d1bfcab0aa4207becbdb
SlinkySlinky <slinkislinki@gmail.com>
2023-01-24 13:36:34 +0300

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors"
 import fs from "fs"
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
